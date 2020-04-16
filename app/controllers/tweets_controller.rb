86a64e1e16fc003c9127d1ef8d6b38b50fdb4bd8
chmellado <ch.mellado@inmade.cl>
2020-04-15 21:35:25 -0400

    navbar

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,5 @@
 class TweetsController < ApplicationController
   def index
     @tweets = Tweet.order(:id).page params[:page]
-
   end
 end
---
+++ db/migrate/20200416005858_add_file_to_user.rb
@@ -0,0 +1,5 @@
+class AddFileToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :photo, :string
+  end
+end
